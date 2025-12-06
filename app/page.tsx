80a3529c293d966e3034c2f5eeea8ef8ae5b1951
ConstintCreations <ethanglenn1208@gmail.com>
2025-12-06 11:11:39 -0600

    Shapelers are now combined into one component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import ColorShapelerBody from "./components/colorShapelerBody";
-import ShapelerEyes from "./components/shapelerEyes";
-import ShapelerEyebrows from "./components/shapelerEyebrows";
-import ShapelerMouth from "./components/shapelerMouth";
+import Shapeler from "./components/shapeler";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="absolute">
-        <ShapelerMouth type="Sprickler" mouth="Sad" />
-        <ShapelerEyebrows type="Sprickler" eyebrow="Worried" />
-        <ShapelerEyes type="Sprickler" male={true} pupil="Crazy" />
-        <ColorShapelerBody color="#b787e2ff" type="Sprickler" />
-      </div>
+        <Shapeler color="#b787e2ff" type="Googler" mouth="Small-Wave" eyebrow="Confused" pupil="Normal" male={true} />
     </div>
   );
 }
